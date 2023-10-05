e1a3f3d77640b82e258e5f27ff26c9e4c22aba04
segsell <gsell.sebastian@gmail.com>
2023-10-05 14:38:46 +0200

    Add dummy example

---
+++ src/parental_care/data_management/task_merge_data.py
@@ -0,0 +1,23 @@
+"""Tasks for merging the data."""
+import pandas as pd
+import pytask
+from parental_care.config import BLD
+from parental_care.config import SRC
+
+
+@pytask.mark.depends_on(
+    {
+        "data": SRC / "data" / "sharew1" / "sharew1_rel8-0-0_cv_r.dta",
+    },
+)
+@pytask.mark.produces(BLD / "data" / "data_merged.csv")
+def task_merge_waves(depends_on, produces):  # noqa: ARG001
+    """Merge the data from the different waves."""
+    merged = pd.DataFrame(
+        {
+            "name": ["Raphael", "Donatello"],
+            "mask": ["red", "purple"],
+            "weapon": ["sai", "bo staff"],
+        },
+    )
+    merged.to_csv(produces, index=False)
