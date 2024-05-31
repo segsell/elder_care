08b012ab8bbaf15c31f8f0c664071f9ba06c52f0
segsell <gsell.sebastian@gmail.com>
2024-05-31 12:34:31 +0200

    Fix precommit issues

--- src/elder_care/model/task_create_model_dictionary.py
+++ src/elder_care/model/task_create_model_dictionary.py
@@ -16,9 +16,6 @@ def task_assemble_exog_processes(
     path_to_health_transition_female: Path = BLD
     / "model"
     / "exog_health_transition_female.pkl",
-    # path_to_care_single_mother: Path = BLD / "model" / "exog_care_single_mother.pkl",
-    # path_to_care_single_father: Path = BLD / "model" / "exog_care_single_father.pkl",
-    # path_to_care_couple: Path = BLD / "model" / "exog_care_couple.pkl",
     path_to_save: Annotated[Path, Product] = BLD / "model" / "exog_processes.pkl",
 ):
     """Assemble exogenous processes dictionaries.
@@ -29,6 +26,15 @@ def task_assemble_exog_processes(
 
     No care demand atm! Implicit by medium and bad health of mother.
 
+    # path_to_care_single_mother: Path = BLD / "model" / "exog_care_single_mother.pkl",
+    # path_to_care_single_father: Path = BLD / "model" / "exog_care_single_father.pkl",
+    # path_to_care_couple: Path = BLD / "model" / "exog_care_couple.pkl",
+
+    exog_care_single_mother = load_dict_from_pickle(path_to_care_single_mother)
+    exog_care_single_father = load_dict_from_pickle(path_to_care_single_father)
+    exog_care_couple = load_dict_from_pickle(path_to_care_couple)
+    _exog_care = exog_care_single_mother | exog_care_single_father | exog_care_couple
+
     """
     exog_survival_prob_female = load_dict_from_pickle(path_to_survival_prob_female)
     _exog_survival_prob = exog_survival_prob_female
@@ -37,11 +43,6 @@ def task_assemble_exog_processes(
         path_to_health_transition_female,
     )
     _exog_health_transition = exog_health_transition_female
-
-    # exog_care_single_mother = load_dict_from_pickle(path_to_care_single_mother)
-    # exog_care_single_father = load_dict_from_pickle(path_to_care_single_father)
-    # exog_care_couple = load_dict_from_pickle(path_to_care_couple)
-    # _exog_care = exog_care_single_mother | exog_care_single_father | exog_care_couple
 
     exog_all = _exog_survival_prob | _exog_health_transition
 
