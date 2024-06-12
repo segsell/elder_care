77c7486dec3a656b5079bddaddb57e4263a0de97
segsell <gsell.sebastian@gmail.com>
2024-06-12 16:26:07 +0200

    Sunab does not work in this setup

--- src/elder_care/event_study/event_study_parents.R
+++ src/elder_care/event_study/event_study_parents.R
@@ -18,6 +18,9 @@ FEMALE <- 2
 data <- read.csv('/home/sebastian/Projects/elder_care/bld/event_study/sandbox_parents.csv')
 data <- subset(data, distance_to_treat >= -6 & distance_to_treat <= 12)
 
+table(data$int_year)
+
+
 table(data$informal_care_child)
 # 0    1
 # 3609  921
@@ -59,6 +62,17 @@ dat <- dat %>%
 
 table(dat$informal_care_daughter)
 
+#dat <- dat %>%
+#  group_by(mergeid) %>%
+#  mutate(distance_to_treat = if_else(treat_ever == 0, NA_real_, distance_to_treat)) %>%
+#  ungroup()
+dat <- dat %>%
+  group_by(mergeid) %>%
+  mutate(year_treated = int_year[distance_to_treat == 0][1]) %>%
+  ungroup()
+
+table(dat$year_treated, dat$distance_to_treat)
+table(dat$int_year)
 
 x_min_value <- -10
 x_max_value <- 10
@@ -155,3 +169,34 @@ iplot(mod_twfe_mother,
       x.lim = c(x_min_value, x_max_value),
       y.lim = c(-10000, 100000))
 summary(mod_twfe_mother)
+
+
+
+#########
+# Sunab #
+#########
+
+# DOES NOT WORK HERE WITH DIFFERENTIAL YEAR BINNING
+
+df = fread("https://raw.githubusercontent.com/LOST-STATS/LOST-STATS.github.io/master/Model_Estimation/Data/Event_Study_DiD/bacon_example.csv") 
+
+dat_all$yyear_treated <- copy(dat_all$year_treated)
+setDT(dat_all)
+dat_all[, year_treated := ifelse(treat_ever==0, 10000, yyear_treated)]
+#dat_mother[, binned_distance_to_treat := ifelse(treat_ever==0, 10000, binned_distance_to_treat)]
+
+mod_twfe_all = feols(informal_care_daughter ~ sunab(year_treated, int_year)
+                     |
+                       mergeid,                         ## FEs
+                     cluster = ~mergeid,   ## Clustered SEs
+                     weights = ~hh_weight,
+                     data = dat_all
+)
+iplot(mod_twfe_all,
+      xlab = 'time to event (mother or father in bad health)',
+      ylab = 'informal care by daughter',
+      main = '',
+      x.lim = c(x_min_value, x_max_value),
+      y.lim = c(-10000, 100000))
+summary(mod_twfe_all)
+
